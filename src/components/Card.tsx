dfefc6fe5699d8dd3f21a63679b5057e1b733365
Tobias Leinss <caruso33@web.de>
2022-09-15 18:10:38 +0200

    Start character overview

---
+++ src/components/Card.tsx
@@ -0,0 +1,22 @@
+type CardProps = {
+  name: string
+  description: string
+  content?: string | React.ReactNode
+  onClick?: () => void
+}
+
+const Card = ({ name, description, content, onClick }: CardProps) => {
+  return (
+    <section
+      onClick={onClick}
+      className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105 cursor-pointer"
+    >
+      <h2 className="text-lg text-gray-700">{name}</h2>
+      <p className="mt-5 text-sm text-gray-600">{description}</p>
+
+      {content}
+    </section>
+  )
+}
+
+export default Card
---
+++ src/pages/character-overview.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next"
+
+const CharacterOverview: NextPage = () => {
+  return <div>character-overview</div>
+}
+
+export default CharacterOverview
